dd78df56e3a69744f53b0e621cd673e183e36b54
Ivan Gormatov <113637583+Ivan-Gormatov@users.noreply.github.com>
2023-02-12 23:54:07 +0300

    Add files via upload

---
+++ news/views.py
@@ -0,0 +1,15 @@
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView # импортируем класс, который говорит нам о том, что в этом представлении мы будем выводить список объектов из БД
+from .models import Post
+ 
+ 
+class NewsList(ListView):
+    model = Post  # указываем модель, объекты которой мы будем выводить
+    template_name = 'news.html'  # указываем имя шаблона, в котором будет лежать HTML, в нём будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'news'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Post.objects.order_by('-dateCreat')
+ 
+class PostDetail(DetailView):
+    model = Post # модель всё та же, но мы хотим получать детали конкретно отдельного товара
+    template_name = 'post.html' # название шаблона будет product.html
+    context_object_name = 'post' # название объекта
